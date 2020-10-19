6d994138474de3df47b32e801b0dbcec32c6cdbd
Daniel Homoncik <homoncikdan@gmail.com>
2020-10-20 01:18:54 +0200

    mines, counting neighbors,

--- cell.js
+++ cell.js
@@ -3,26 +3,36 @@ class Cell {
     constructor(isMine, x, y, size) {
         this.mine = isMine;
         this.neighborCounter = neighbord;
-        this.revealed = false;
+        this.revealed = revealed;
         this.x = x;
         this.y = y;
         this.size = size;
-
+        this.lost = lost;
     }
 
+    
     show() {
-        fill('white');
-        rect(this.x, this.y, this.size);
-        if (!this.mine) {
-            let c = color('red');
-            fill(c);
-            ellipse((cellSize * 0.5) + (cellSize * x), (cellSize * 0.5) + (cellSize * y), cellSize * 0.6);
+        fill('grey')
+        text('refres', cellSize * 0.5 + (cellSize * x), (cellSize * 0.5) + (cellSize * y), cellSize);
+        
+        if (board[y][x].revealed === true) {
+            fill('gray');
+            rect(this.x, this.y, this.size);
         }
         else {
-            fill('black')
-            text(this.neighborCounter, cellSize * 0.5 + (cellSize * x), (cellSize * 0.5) + (cellSize * y), cellSize);
+            fill('blue');
+            rect(this.x, this.y, this.size);
 
+            if (!this.mine) {
+                let c = color('red');
+                fill(c);
+                ellipse((cellSize * 0.5) + (cellSize * x), (cellSize * 0.5) + (cellSize * y), cellSize * 0.6);
+                lost++;
+            }
+            else {
+                fill('black')
+                text(this.neighborCounter, cellSize * 0.5 + (cellSize * x), (cellSize * 0.5) + (cellSize * y), cellSize);
+            }
         }
     }
-
 }
--- mineSweeper.js
+++ mineSweeper.js
@@ -3,7 +3,8 @@ var ySize = 10;
 var cellSize = 30;
 var board;
 var neighbord = 0;
-
+let revealed = true;
+var lost = 0;
 function createBoard() {
     board = new Array(ySize);
     for (let y = 0; y < ySize; y++) {
@@ -13,7 +14,6 @@ function createBoard() {
         }
     }
 }
-
 function counterNeighbors() {
     for (y = 0; y < ySize; y++) {
         for (x = 0; x < xSize; x++) {
@@ -45,10 +45,16 @@ function counterNeighbors() {
             }
         }
     }
-
-
-    function newFunction() {
-        return this.mine;
+}
+function mouseClicked() {
+    if (this.mine = true) {
+        for (y = 0; y < ySize; y++) {
+            for (x = 0; x < xSize; x++) {
+                if (lost < 1 && mouseX >= board[y][x].x && mouseX <= (board[y][x].x + cellSize) && mouseY >= board[y][x].y && mouseY <= (board[y][x].y + cellSize)) {
+                    board[y][x].revealed = false;
+                }
+            }
+        }
     }
 }
 function setup() {
@@ -56,11 +62,10 @@ function setup() {
     createBoard();
     counterNeighbors();
 }
-
 function draw() {
     for (y = 0; y < ySize; y++) {
         for (x = 0; x < xSize; x++) {
-            board[y][x].show();
+            board[y][x].show()
         }
     }
 }
